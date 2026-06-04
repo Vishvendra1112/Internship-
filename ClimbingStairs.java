caccf870fa7d998d57542d65c4321faf0384970a
Vishvendra1112 <vishvendradhama10@gmail.com>
2026-06-04 15:10:41 +0530

    Q70 ClimbingStairs

---
+++ ClimbingStairs.java
@@ -0,0 +1,14 @@
+class  ClimbingStairs{
+    public static int climbStairs(int n) {
+       if(n < 2){
+        return 1;
+
+       } 
+       return climbStairs(n -1) + climbStairs(n -2); 
+    }
+    public static void main(String[] args) {
+     
+        System.out.println(climbStairs(5));                         
+
+    }
+}
