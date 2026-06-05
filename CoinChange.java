f6401519f06cb28f823589cc1f2b0b2dd0e2fab6
Vishvendra1112 <vishvendradhama10@gmail.com>
2026-06-05 11:10:38 +0530

    Q 322 Coin Change

---
+++ CoinChange.java
@@ -0,0 +1,20 @@
+class CoinChange {
+    public static int minCoins(int[] coins , int amount){
+        if(amount == 0) return 0;
+        if(amount <0) return Integer.MAX_VALUE;
+
+        int minimum = Integer.MAX_VALUE;
+        for(int i=0;i<coins.length;i++){
+            int newAmount = minCoins(coins,amount-coins[i]);
+        if(newAmount != Integer.MAX_VALUE) {
+            minimum = Math.min(newAmount+1,minimum);
+        }   
+        }
+        return minimum;
+    }
+    public int coinChange(int[] coins, int amount) {
+        int newAmount = minCoins(coins, amount);
+        return (newAmount == Integer.MAX_VALUE)?-1: newAmount;
+        
+    }
+}
