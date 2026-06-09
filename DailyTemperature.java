94c557774ae1bc39b173586cd9dee8748d1f670e
Vishvendra1112 <vishvendradhama10@gmail.com>
2026-06-09 16:03:29 +0530

    Q 739 DailyTemperature

--- DailyTemperature.java
+++ DailyTemperature.java
@@ -1,3 +1,19 @@
+import java.util.Stack;
+
 public class DailyTemperature {
     
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        Stack<Integer> st = new Stack <>();
+        int[] result = new int[n];
+
+        for(int i = n - 1;i>=0;i--){
+            while(!st.isEmpty() && temperatures[i]>= temperatures[st.peek()])
+             st.pop();
+            if(!st.isEmpty())
+            result[i] = st.peek() - i;
+            st.push(i);      
+        }
+        return result;
+    }
 }
