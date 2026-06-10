f4360c4f7077632cb4fd28fa3efc33e356189e58
Vishvendra1112 <vishvendradhama10@gmail.com>
2026-06-10 11:44:52 +0530

    Q  543. Diameter of Binary Tree

--- DiameterofBinaryTree.java
+++ DiameterofBinaryTree.java
@@ -1,3 +1,23 @@
+import javax.swing.tree.TreeNode;
+
 public class DiameterofBinaryTree {
-    
+    public static int traversal(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int leftMax = traversal(root.left);
+        int rightMax = traversal(root.right);
+
+        int max = Math.max(leftMax, rightMax)+1;
+        return max;
+    }
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root == null){
+            return 0 ;
+        }
+        int dia1 = diameterOfBinaryTree(root.left);
+        int dia2 = diameterOfBinaryTree(root.right);
+        int dia3 = traversal(root.left)+traversal(root.right);
+        return Math.max(dia3,Math.max(dia1,dia2));
+    }
 }
